6046fd43371381db3b845ce67ce3abea5c679ef3
Xiahui Liu <98664540+newbieherecs@users.noreply.github.com>
2023-03-29 14:50:06 +0200

    update controller

--- part4/controller/login.js
+++ part4/controller/login.js
@@ -25,12 +25,14 @@ loginRouter.post("/", async (request, response) => {
     id: user._id,
   };
 
+  console.log(userForToken);
 
-  const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: 60 * 60 });
+  const token = jwt.sign(userForToken, process.env.SECRET);
+  console.log(token);
 
   response
     .status(200)
-    .send({ token, username: user.username, name: user.name });
+    .send({token, username: user.username, name: user.name });
 });
 
 module.exports = loginRouter;
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -3,38 +3,7 @@ const _ = require("lodash");
 const dummy = (blogs) => {
   return 1;
 };
-// const listBlogs = [
-//   {
-//     _id: "5a422aa71b54a676234d17f8",
-//     title: "Go To Statement Considered Harmful",
-//     author: "Edsger W. Dijkstra",
-//     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-//     likes: 5,
-//     __v: 0
-//   },
-//   {
-//     title: "Canonical string reduction",
-//     author: "Alex",
-//     likes: 12,
-//     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-//   },
-//   {
-//     title: "nwe blog",
-//     author: "Ivan Ivanov",
-//     likes: 11
-//   },
-//   {
-//     title: "fun blog",
-//     author: "Ivan Ivanov",
-//     likes: 19,
-//     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-//   },
-//   {
-//     title: "fun big blog",
-//     author: "Alex",
-//     likes: 19,
-//   }
-// ];
+
 const totalLikes = (blogs) => {
   const reducer = (sum, blog) => {
     return sum + blog.likes;
